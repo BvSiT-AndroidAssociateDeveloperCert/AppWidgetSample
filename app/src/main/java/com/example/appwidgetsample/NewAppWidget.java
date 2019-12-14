15d9c6eca9924d987cb9c04365e0878f950c24cd
BvSiT <bvstraten1@gmail.com>
2019-12-14 17:41:27 +0100

    Solution

--- app/src/main/java/com/example/appwidgetsample/NewAppWidget.java
+++ app/src/main/java/com/example/appwidgetsample/NewAppWidget.java
@@ -15,12 +15,12 @@ public class NewAppWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
-
         // Instruct the widget manager to update the widget
+
+        views.setTextViewText(R.id.appwidget_id,String.valueOf(appWidgetId));
+
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
@@ -32,14 +32,5 @@ public class NewAppWidget extends AppWidgetProvider {
         }
     }
 
-    @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-    }
-
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
-    }
 }
 
