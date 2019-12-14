9c148602726e5e68285f0027d83f2cd0a2c5f99d
BvSiT <bvstraten1@gmail.com>
2019-12-14 19:39:05 +0100

    Solution

--- app/src/main/java/com/example/appwidgetsample/NewAppWidget.java
+++ app/src/main/java/com/example/appwidgetsample/NewAppWidget.java
@@ -1,16 +1,29 @@
 package com.example.appwidgetsample;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.Preference;
 import android.widget.RemoteViews;
 
 import com.example.appwidgetsample.R;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.prefs.Preferences;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class NewAppWidget extends AppWidgetProvider {
+
+    private static final String mSharedPrefFile =
+            "com.example.appwidgetsample";
+    private static final String COUNT_KEY = "count";
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
@@ -21,6 +34,29 @@ public class NewAppWidget extends AppWidgetProvider {
 
         views.setTextViewText(R.id.appwidget_id,String.valueOf(appWidgetId));
 
+        SharedPreferences prefs = context.getSharedPreferences(mSharedPrefFile,0);
+        int count = prefs.getInt(COUNT_KEY+appWidgetId,0);
+        count++;
+        prefs.edit().putInt(COUNT_KEY+appWidgetId,count).apply();
+
+        String dateString =
+                DateFormat.getDateTimeInstance(DateFormat.FULL,DateFormat.FULL).format(new Date());
+
+        views.setTextViewText(R.id.appwidget_update,
+                context.getResources().getString(
+                        R.string.date_count_format,count,dateString));
+
+        Intent intent = new Intent(context,NewAppWidget.class);
+        //intent.setAction("android.appwidget.action.APPWIDGET_UPDATE");
+        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        int[] idArray = new int[]{appWidgetId};
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS,idArray);
+
+        PendingIntent p = PendingIntent.getBroadcast(context,appWidgetId,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        views.setOnClickPendingIntent(R.id.button_update,p);
+
+        //BvS This line AFTER setting the update intent!!
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
